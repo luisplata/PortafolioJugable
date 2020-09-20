ebf38dbbaa10035e20639db524d0b0309e82198c
Luis Plata <www.luisplata@gmail.com>
2020-09-20 14:34:51 -0500

    Agregando los primeros assets de la novela visual, y primeras pruebas de los dialogos

---
+++ Assets/NovelaGrafica/Scripts/ControladorDeEscenaNovelaVisual.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorDeEscenaNovelaVisual : MonoBehaviour
+{
+    public GameObject panelOpciones, opcion1, opcion2, botonSiguienteGO;
+
+    public Button botonSiguiente;
+
+    public TextMeshProUGUI texto;
+
+    private void Start()
+    {
+        botonSiguiente = botonSiguienteGO.GetComponent<Button>();
+    }
+}
